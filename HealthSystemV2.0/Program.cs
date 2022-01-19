fa318629110de9c1803ab9dd591b9a9e74f096a1
Kyle Dixon <90637263+KyleDixonVO@users.noreply.github.com>
2022-01-19 19:31:07 -0400

    new methods in main and player
    added methods for testing in main, added methods: reset(), die(), respawn(),  heal(), and regenShield() to player

--- HealthSystemV2.0/Program.cs
+++ HealthSystemV2.0/Program.cs
@@ -11,6 +11,11 @@ namespace HealthSystemV2._0
         public int health = 100;
         public int lives = 3;
         public int shield = 100;
+        public int defaultHealth = 100;
+        public int defaultShield = 100;
+        public int defaultLives = 3;
+        public bool dead = false;
+
         private int remainder;
 
         public void TakeDamage(int damage)
@@ -18,6 +23,7 @@ namespace HealthSystemV2._0
             Console.WriteLine("Incoming damage: " + damage);
             if (damage >= 0)
             {
+                remainder = 0;
                 shield = shield - damage;
                 if (shield < 0 )
                 {
@@ -30,15 +36,17 @@ namespace HealthSystemV2._0
                     health = health + remainder;
                     if (health <= 0)
                     {
-                        lives--;
-                        Console.WriteLine("Player lost a life!");
+                        health = 0;
+                        Die();
                     }
                 }
                
             }
             else
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Damage must be a positive integer, or zero.");
+                Console.ResetColor();
             }
            
             
@@ -46,19 +54,193 @@ namespace HealthSystemV2._0
 
         public void ShowHud()
         {
-            Console.WriteLine("Health: " + health);
+            Console.WriteLine("");
+            //Console.WriteLine("Player: " + name.ToString());
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("Shield: " + shield);
+            Console.WriteLine("Health: " + health);
             Console.WriteLine("Lives: " + lives);
+            Console.ResetColor();
+            Console.WriteLine("");
+        }
+
+        public void Reset()
+        {
+            health = defaultHealth;
+            shield = defaultShield;
+            lives = defaultLives;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Reset.");
+            Console.ResetColor();
+        }
+
+        public void Respawn()
+        {
+            health = defaultHealth;
+            shield = defaultShield;
+            Console.WriteLine("Player respawned!");
+        }
+
+        public void Die()
+        {
+            ShowHud();
+            Console.WriteLine("Player lost a life!");
+            if (lives > 0)
+            {
+                Respawn();
+            }
+            else
+            {
+                Console.WriteLine("Out Of lives!");
+                lives = 0;
+                dead = true;
+                return;
+            }
+            lives--;
+        }
+
+        public void Heal(int HP)
+        {
+            if (HP >= 0 )
+            {
+                Console.WriteLine("Incoming healing: " + HP);
+                health = health + HP;
+                if (health > 100)
+                {
+                    int excess;
+                    excess = health - 100;
+                    int amountHealed;
+                    amountHealed = HP - excess;
+                    Console.WriteLine("Healed for: " + amountHealed);
+                    health = 100;
+                    Console.WriteLine("Cannot be healed further.");
+                }
+                else
+                {
+                    Console.WriteLine("Healed for: " + HP);
+                    if (health == 100)
+                    {
+                        Console.WriteLine("Cannot be healed further.");
+                    }
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("HP must be a positive integer or zero.");
+                Console.ResetColor();
+            }
+                
+        }
+
+        public void RegenShield(int SP)
+        {
+            if (SP >= 0)
+            {
+                Console.WriteLine("Regenerating shield: " + SP);
+                shield = shield + SP;
+                if (shield > 100)
+                {
+                    int excess;
+                    excess = shield - 100;
+                    int amountRegen;
+                    amountRegen = SP - excess;
+                    Console.WriteLine("Regenerated shield by: " + amountRegen);
+                    shield = 100;
+                    Console.WriteLine("Cannot be reinforced further.");
+                }
+                else
+                {
+                    Console.WriteLine("Regenerated shield by: " + SP);
+                    if (shield == 100)
+                    {
+                        Console.WriteLine("Cannot be reinforced further.");
+                    }
+                }
+            }
+            else
+            {   
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("SP must be a positive integer or zero.");
+                Console.ResetColor();
+            }
         }
     }
 
     class Program
     {
+
+        public bool gameOver = false;
         static void Main(string[] args)
         {
             Player player = new Player();
-            player.TakeDamage(35);
+            damageTest(player, 35);
+            damageTest(player, 120);
+            damageTest(player, 200);
+            damageTest(player, -10);
+            respawnTest(player);
+            healTest(player, 50, 30);
+            healTest(player, 30, 50);
+            healTest(player, 0, -10);
+            regenTest(player, 50, 30);
+            regenTest(player, 30, 50);
+            regenTest(player, 0, -10);
+            Console.ReadKey(true);
         }
 
+        static void damageTest(Player name, int damage)
+        {
+            Console.WriteLine("Damage test.");
+            name.ShowHud();
+            name.TakeDamage(damage);
+            name.ShowHud();
+            name.Reset();
+            Console.ReadKey(true);
+            Console.Clear();
+        }
+
+        static void healTest(Player name, int damage, int HP)
+        {
+            Console.WriteLine("Healing test.");
+            name.shield = 0;
+            name.ShowHud();
+            name.TakeDamage(damage);
+            name.ShowHud();
+            name.Heal(HP);
+            name.ShowHud();
+            name.Reset();
+            Console.ReadKey(true);
+            Console.Clear();
+        }
+
+        static void regenTest(Player name, int damage, int SP)
+        {
+            Console.WriteLine("Shield regen test.");
+            name.ShowHud();
+            name.TakeDamage(damage);
+            name.ShowHud();
+            name.RegenShield(SP);
+            name.ShowHud();
+            name.Reset();
+            Console.ReadKey(true);
+            Console.Clear();
+        }
+
+        static void respawnTest(Player name)
+        {
+            Console.WriteLine("Respawn test.");
+            name.ShowHud();
+            name.TakeDamage(200);
+            name.ShowHud();
+            name.TakeDamage(200);
+            name.ShowHud();
+            name.TakeDamage(200);
+            name.ShowHud();
+            name.TakeDamage(200);
+            name.ShowHud();
+            name.Reset();
+            Console.ReadKey(true);
+            Console.Clear();
+        }
     }
 }
